50ed49290207d10321ba4d683c52301d5a7e6065
Anders Kleimark <anders.kleimark@proton.me>
2026-06-17 00:03:44 +0200

    Tillägg av meny, huvudfönster med mera.

--- main_window.py
+++ main_window.py
@@ -1,10 +1,12 @@
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction
+from PySide6.QtGui import QFont
 from PySide6.QtWidgets import (
     QMainWindow,
-    QTabWidget,
+    QTableWidget,
     QWidget,
     QVBoxLayout,
-    QLabel,
+    QLabel
 )
 
 
@@ -14,4 +16,37 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("FIDE Norm Calculator")
-        self.resize(1000, 700)
+        self.resize(800, 600)
+        self.create_menu_system()
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout()
+        self.central_widget.setLayout(self.layout)
+        self.create_main_header()
+        self.create_main_table()
+
+    def create_menu_system(self):
+        menu_bar = self.menuBar()
+        # FileMenu
+        file_menu = menu_bar.addMenu("File")
+        exit_action = QAction("Quit", self)
+        exit_action.triggered.connect(self.close)
+        file_menu.addAction(exit_action)
+
+    def create_main_header(self):
+        self.header = QLabel("Players")
+        self.layout.addWidget(self.header)
+
+        # Centrera texten
+        self.header.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Gör texten större och fet
+        font = QFont()
+        font.setPointSize(20)
+        font.setBold(True)
+        self.header.setFont(font)
+        self.layout.addWidget(self.header)
+
+    def create_main_table(self):
+        self.table = QTableWidget(3, 5)
+        self.layout.addWidget(self.table)
