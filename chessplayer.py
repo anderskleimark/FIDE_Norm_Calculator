a296089042205a5f173c3930dc32be629f3f81a4
Anders Kleimark <anders.kleimark@proton.me>
2026-06-18 11:24:34 +0200

    Borttagning av chessplayer.py. Klassen inlagd i main_window.py.

--- chessplayer.py
+++
@@ -1,7 +0,0 @@
-class Chessplayer:
-    def __init__(self):
-        self.firstname = ""
-        self.lastname = ""
-        self.federation = ""
-        self.rating = ""
-        self.title = None
